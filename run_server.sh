1b1e77a1972a530334f9514faf26fe46da05cdf2
hshin-git <hiromasa.shin@gmail.com>
2020-05-23 09:35:32 +0900

    make layer in run_server

--- run_server.sh
+++ run_server.sh
@@ -3,5 +3,6 @@ mkdir layer
 ln -s ../gfs_rank/info .
 ln -s ../gfs_rank/graph .
 ln -s ../gfs_rank/pub/tenki .
+python 3_sdp_to_layer.py
 python -m http.server 8000
 #google-chrome http://localhost:8000/
